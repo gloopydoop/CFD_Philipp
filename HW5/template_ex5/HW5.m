3699db1aa8625477ddc228be95d5abed03282cf3
gloopydoop <nobis@mech.kth.se>
2020-02-17 14:32:41 +0100

    computer about to die!

--- HW5/template_ex5/HW5.m
+++ HW5/template_ex5/HW5.m
@@ -43,25 +43,39 @@ omega = 1.9;                             % SOR parameter
 % grid on
 
 %% Third part
-Nx = 30:10:90; % my laptop ran out of memory at 100...
-Ny = Nx;
-figure()
-for i =1:length(Nx)
-    [L,f,hx,hy,X,Y] = setup_L_f(Nx(i),Ny(i));
-    p0 = zeros(size(f));
-    [r,p] = GS_SOR(1, Nx(i), Ny(i), hx, hy, L, f, p0, tol, maxit);
-    semilogy(r)
-    leg{i} = ['Nx = Ny = ',num2str(Nx(i))];
-    hold on
-end
-
-xlabel('Iteration')
-ylabel('Residual')
-legend(leg)
-grid on
+% Nx = 30:10:90; % my laptop ran out of memory at 100...
+% Ny = Nx;
+% figure()
+% for i =1:length(Nx)
+%     [L,f,hx,hy,X,Y] = setup_L_f(Nx(i),Ny(i));
+%     p0 = zeros(size(f));
+%     [p,r] = GS_SOR(1, Nx(i), Ny(i), hx, hy, L, f, p0, tol, maxit);
+%     semilogy(r)
+%     leg{i} = ['Nx = Ny = ',num2str(Nx(i))];
+%     hold on
+% end
+% 
+% xlabel('Iteration')
+% ylabel('Residual')
+% legend(leg)
+% grid on
 
 %% Forth part
-% [p, flag, endrelres, iter, reshist] = gmres(L, f, [], tol);
+tol   = 10^-6;
+maxit = 1000;
 
+[L,f,hx,hy,X,Y] = setup_L_f(Nx,Ny);
+p0 = zeros(size(f));
+[p, flag, endrelres, iter, reshist] = gmres(L, f, [], 10^-6,maxit);
+semilogy(reshist/norm(f))
+[p,r] = GS_SOR(1, Nx, Ny, hx, hy, L, f, p0, tol, maxit);
+hold on
+semilogy(r)
+[p,r] = GS_SOR(1.9, Nx, Ny, hx, hy, L, f, p0, tol, maxit);
+semilogy(r)
+xlabel('Iteration')
+ylabel('Residual')
+grid on
+legend('gmres','GS','SOR')
 %-----------------------------------------
 % plots
